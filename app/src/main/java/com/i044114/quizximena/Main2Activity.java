77e06345ab976a601f462119002d561dcc3d3f6b
ximena <xime.huertas@rocketmail.com>
2017-08-24 13:46:43 -0500

    cambios

--- app/src/main/java/com/i044114/quizximena/Main2Activity.java
+++ app/src/main/java/com/i044114/quizximena/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.i044114.quizximena;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public void goActivityuno (View v){
+        Intent intentun=new Intent(this, MainActivity.class);
+        startActivity(intentun);
     }
 }
--- app/src/main/java/com/i044114/quizximena/MainActivity.java
+++ app/src/main/java/com/i044114/quizximena/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         FloatingActionButton fab =(FloatingActionButton) findViewById(R.id.fab_1);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
