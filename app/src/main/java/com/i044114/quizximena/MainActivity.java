8b4b0a22a47fcff0bc0e6fe37b27949a6fbe9692
ximena <xime.huertas@rocketmail.com>
2017-08-24 13:33:53 -0500

    Initial commit

---
+++ app/src/main/java/com/i044114/quizximena/MainActivity.java
@@ -0,0 +1,27 @@
+package com.i044114.quizximena;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FloatingActionButton fab =(FloatingActionButton) findViewById(R.id.fab_1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goActivity(view);
+            }
+        });
+    }
+    public void goActivity (View v){
+        Intent intent=new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+}
